591ae35c9bce3c37c3514eed8fcfec74501ab1d8
nishanth <nishanth@codeastra.com>
2018-03-16 16:49:02 +0530

    publish changes

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ var config = require('./config');
 
 var port = process.env.PORT || 3000;
 
+console.log('Here', port)
 app.use('/assets', express.static(__dirname + '/public'));
+app.get('/', (req, res) => res.send('Hello World!'))
 
 app.set('view engine', 'ejs');
 
 mongoose.connect(config.getDbConnectionString());  
-
-app.listen(port);
+app.listen(port, () => console.log('Example app listening on port 3000!'));
